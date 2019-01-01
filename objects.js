a3376292dbee4d90a84dfcff43368f9977525a81
shawn a meighn <smeighn@gmail.com>
2019-01-01 18:52:55 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,78 @@
+var cart =[];
+    
+    function addToCart(item){
+      var newObject= {};
+        newObject[item]=Math.floor(Math.random()*100);
+      
+     cart.push(newObject)
+      console.log(`${item} has been added to your cart.`);
+      return cart;
+    
+    }
+    
+  addToCart("bananas");
+  addToCart("egg");
+  addToCart("strawberries");
+  
+  function viewCart() {
+  var sentence_starter = "In your cart, you have";
+  var cart_list = [];
+  
+ cart.forEach(function(item) {
+    for(var key in item){
+        cart_list.push(`${key} at $${item[key]}`)
+    }
+ });
+       if(cart_list.length===0){
+         console.log("Your shopping cart is empty.")
+       }
+      else if(cart_list.length == 1){
+      console.log(`${sentence_starter} ${cart_list}.`);
+       }
+       else if(cart_list.length ==2){
+         console.log(`${sentence_starter} ${cart_list[0]} and ${cart_list[1]}.`);
+       }
+       else if(cart_list.length>=3){
+         cart_list[cart_list.length-1]= "and "+cart_list[cart_list.length-1];
+         console.log(`${sentence_starter} ${cart_list.join(", ")}.`);
+       }
+    
+}
+
+function total() {
+  var total = 0;
+  cart.forEach(function(item){
+    for(var key in item){
+      total+=item[key];
+    }
+  });
+  return total;
+}
+function removeFromCart(item) {
+   var foodChecker= false;
+  for(var i = 0;i<cart.length;i++){
+    if(cart[i].hasOwnProperty(item)){
+        foodChecker=true;
+        cart.splice(i,1);
+    
+ }
+}
+  
+   if(!foodChecker){
+    console.log("That item is not in your cart.");
+  }
+    
+  
+  return cart;
+}
+    console.log(removeFromCart("eg"));
+
+function placeOrder(cardNumber) {
+  if(arguments.length===0){
+    console.log("Sorry , we don't have a credit card on file for you.");
+  }
+  else if(arguments.length ==1){
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`);
+    cart = [];
+  }
+}
